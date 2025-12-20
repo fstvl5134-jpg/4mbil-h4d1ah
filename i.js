782e3317d05d4eb98ceb98f4827499fad6fcbd96
fstvl5134-jpg <fstvl5134@gmail.com>
2025-12-20 09:17:48 +0700

    Create i.js

---
+++ i.js
@@ -0,0 +1,6 @@
+$(document).ready(function() {
+    $('form').on('submit', function(e) {
+        // Jika kamu ingin pindah halaman SETELAH tombol diklik:
+        window.location.href = "proses.html";
+    });
+});
